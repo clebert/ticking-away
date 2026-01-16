ee345a63389e4dcc379230293aacf7a88ada3f74
Clemens Akens <clebert@me.com>
2026-01-16 21:28:39 +0000

    Add Floyd-Steinberg dithering modes (2-bit and 4-bit)

--- src/dithering.h
+++ src/dithering.h
@@ -9,7 +9,9 @@
 #define DITHER_MODE_1BIT_ATKINSON 1
 #define DITHER_MODE_2BIT_ATKINSON 2
 #define DITHER_MODE_2BIT_BAYER 3
-#define DITHER_MODE_4BIT_BAYER 4
+#define DITHER_MODE_2BIT_FLOYD_STEINBERG 4
+#define DITHER_MODE_4BIT_BAYER 5
+#define DITHER_MODE_4BIT_FLOYD_STEINBERG 6
 
 // Convert RGB to luminance using standard weights
 static inline int luminance(int r, int g, int b) {
@@ -29,9 +31,11 @@ static inline int quantize_luminance(int lum, int mode) {
 
   if (mode == DITHER_MODE_1BIT_ATKINSON) {
     return (clamped < 128) ? 0 : 255;
-  } else if (mode == DITHER_MODE_2BIT_ATKINSON || mode == DITHER_MODE_2BIT_BAYER) {
+  } else if (mode == DITHER_MODE_2BIT_ATKINSON || mode == DITHER_MODE_2BIT_BAYER ||
+             mode == DITHER_MODE_2BIT_FLOYD_STEINBERG) {
     return ((clamped + 42) / 85) * 85;
   } else {
+    // 4-bit modes: 16 levels
     return ((clamped + 8) / 17) * 17;
   }
 }
@@ -83,6 +87,52 @@ static inline void apply_dithering(uint8_t* fb, int width, int height, int mode)
         fb[idx] = level;
         fb[idx + 1] = level;
         fb[idx + 2] = level;
+      }
+    }
+    return;
+  }
+
+  // Floyd-Steinberg error diffusion (100% error distribution)
+  // Distribution pattern:
+  //     X   7/16
+  // 3/16 5/16 1/16
+  if (mode == DITHER_MODE_2BIT_FLOYD_STEINBERG || mode == DITHER_MODE_4BIT_FLOYD_STEINBERG) {
+    static int fs_curr[DITHER_MAX_WIDTH];
+    static int fs_next[DITHER_MAX_WIDTH];
+
+    // Clear error buffers
+    for (int i = 0; i < width; i++) {
+      fs_curr[i] = 0;
+      fs_next[i] = 0;
+    }
+
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+        int idx = (y * width + x) * 4;
+
+        // Get original luminance and add accumulated error
+        int old_lum = luminance(fb[idx], fb[idx + 1], fb[idx + 2]) + fs_curr[x];
+
+        // Quantize based on mode
+        int new_lum = quantize_luminance(old_lum, mode);
+        int error = old_lum - new_lum;
+
+        fb[idx] = new_lum;
+        fb[idx + 1] = new_lum;
+        fb[idx + 2] = new_lum;
+
+        // Floyd-Steinberg distributes 100% of error:
+        // X+1: 7/16, Y+1 X-1: 3/16, Y+1 X: 5/16, Y+1 X+1: 1/16
+        if (x + 1 < width) fs_curr[x + 1] += error * 7 / 16;
+        if (x > 0) fs_next[x - 1] += error * 3 / 16;
+        fs_next[x] += error * 5 / 16;
+        if (x + 1 < width) fs_next[x + 1] += error * 1 / 16;
+      }
+
+      // Rotate row buffers: next becomes current, clear next
+      for (int i = 0; i < width; i++) {
+        fs_curr[i] = fs_next[i];
+        fs_next[i] = 0;
       }
     }
     return;
